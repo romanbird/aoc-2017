589f91c380d6b46061702baf611f08880791b091
Roman Bird <romanbird@gmx.com>
2018-11-25 13:56:43 +0000

    commit day 5 part 1

--- 05/one.py
+++ 05/one.py
@@ -1,24 +1,18 @@
-""" INPUT READER
 raw = open("input.txt")
 db=[]
 for i in raw.readlines():
     db.append(int(i.strip().replace("\n", "")))
-"""
-db=[0,3,0,1,-3]
-index=0
-current=0
+
+#db=[0,3,0,1,-3] #expected 5
 count=0
-new=0
+index=0
+indexN=0
 while True:
     try:
-        print(db)
-        current=db[index]
-        new=db[index+current]
-        db[current]+=1
+        indexN=db[index]
         index+=db[index]
-
-
+        db[index-indexN]+=1
+        count+=1
     except Exception as e:
-        print(e)
         print(count)
         break
