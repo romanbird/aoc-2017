f2ac7193f1570537e8cc3eb4c2c42b6e209058af
Roman Bird <romanbird@gmx.com>
2018-11-25 18:37:36 +0000

    complete day 2 part 2 using itertools

--- 02/two.py
+++ 02/two.py
@@ -1,5 +1,16 @@
+import itertools
 code = open("input.txt",'r')
 rows = []
 for i in code.readlines():
   i=i.strip().split("\t")
   rows.append([int(n) for n in i])
+
+result=0
+
+for row in rows:
+    for number in itertools.combinations(row, 2):
+        if number[0]%number[1]==0:
+            result+=number[0]//number[1]
+        elif number[1]%number[0]==0:
+            result+=number[1]//number[0]
+print(result)
