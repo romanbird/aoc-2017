0b8f5586e882c39ec569a37729f93d229f454800
Roman Bird <romanbird@gmx.com>
2018-11-25 21:09:06 +0000

    day 4 part 2 complete

--- 04/two.py
+++ 04/two.py
@@ -2,17 +2,21 @@ from collections import Counter
 raw = open("input.txt")
 db=[]
 count=0
-"""
+
+def sortWord(word):
+    return "".join(sorted(word))
+
+
 for passphrase in raw.readlines():
   currentValid=True
-  for i in Counter(passphrase.strip().replace("\n", "").split()).most_common():
+  passphrase=passphrase.strip().replace("\n", "").split()
+  for n,i in enumerate(passphrase):
+      passphrase[n]=sortWord(i)
+  for i in Counter(passphrase).most_common():
     if i[1] != 1:
       currentValid=False
       continue
   if currentValid:
     count+=1
+
 print(count)
-"""
-def sortWord(word):
-    print("".join(sorted(word)))
-sortWord("john")
