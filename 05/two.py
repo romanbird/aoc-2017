0bd597e31b21cfd978260185d1d997a4898c3e9d
Roman Bird <romanbird@gmx.com>
2018-11-25 14:07:01 +0000

    commit day 5 part 2

---
+++ 05/two.py
@@ -0,0 +1,21 @@
+raw = open("input.txt")
+db=[]
+for i in raw.readlines():
+    db.append(int(i.strip().replace("\n", "")))
+
+#db=[0,3,0,1,-3] #expected 5
+count=0
+index=0
+indexN=0
+while True:
+    try:
+        indexN=db[index]
+        index+=db[index]
+        if db[index-indexN]>=3:
+            db[index-indexN]-=1
+        else:
+            db[index-indexN]+=1
+        count+=1
+    except Exception as e:
+        print(count)
+        break
