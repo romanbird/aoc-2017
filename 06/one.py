05b6b928e25981f6cb8d01bc885b3d4b23acd3b4
Roman Bird <romanbird@gmx.com>
2018-11-27 20:46:39 +0000

    moved to main function

--- 06/one.py
+++ 06/one.py
@@ -1,16 +1,17 @@
-x = [int(i) for i in open("input.txt").readlines()[0].split()]
-#x = [0,2,7,0] # test input, expecting 5
-backlog=[]
-while True:
-    maxx=max(x)
-    location=x.index(maxx)
-    x[location]=0
-    i=0
-    while i != maxx:
-        x[(location+1+i)%len(x)]+=1
-        i+=1
-    if tuple(x) in backlog:
-        break
-    backlog.append(tuple(x))
-print(x)
-print(len(backlog)+1)
+def main():
+    x = [int(i) for i in open("input.txt").readlines()[0].split()]
+    #x = [0,2,7,0] # test input, expecting 5
+    backlog=[]
+    while True:
+        maxx=max(x)
+        location=x.index(maxx)
+        x[location]=0
+        i=0
+        while i != maxx:
+            x[(location+1+i)%len(x)]+=1
+            i+=1
+        if tuple(x) in backlog:
+            break
+        backlog.append(tuple(x))
+    return len(backlog)+1
+print(main())
